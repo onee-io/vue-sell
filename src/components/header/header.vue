17a072aba33252b84f689b61c7553a70fb8d2787
OneE <774247851@qq.com>
2017-05-06 19:39:45 +0800

    Completed base header.

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,6 +1,35 @@
 <template>
   <div class="header">
-    I am header.
+    <div class="content-wrapper">
+      <div class="avatar">
+        <img width="64px" height="64px" :src="seller.avatar">
+      </div>
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">{{seller.name}}</span>
+        </div>
+        <div class="description">
+          {{seller.description}}/{{seller.deliveryTime}}分钟送达
+        </div>
+        <div v-if="seller.supports" class="support">
+          <span class="icon" :class="classMap[seller.supports[0].type]"></span>
+          <span class="text">{{seller.supports[0].description}}</span>
+        </div>
+        <div v-if="seller.supports" class="support-count">
+          <span class="count">{{seller.supports.length}}个</span>
+          <i class="icon-keyboard_arrow_right"></i>
+        </div>
+      </div>
+    </div>
+    <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span>
+      <span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
+    </div>
   </div>
 </template>
 
@@ -10,10 +39,128 @@
       seller: {
         type: Object
       }
+    },
+    created () {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
     }
   }
 </script>
 
-<style lang="stylus" rel="stylesheet/stylus">
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  @import "../../common/stylus/mixin.styl"
+
+  .header
+    position relative
+    color #fff
+    background rgba(7, 17, 27, 0.5)
+    .content-wrapper
+      position relative
+      padding 24px 12px 18px 24px
+      font-size 0
+      .avatar
+        display inline-block
+        vertical-align top
+        img
+          border-radius 2px
+      .content
+        display inline-block
+        margin-left 16px
+        .title
+          margin 2px 0 8px 0
+          .brand
+            display inline-block
+            vertical-align top
+            width 30px
+            height 18px
+            bg-image('brand')
+            background-size 30px 18px
+            background-repeat no-repeat
+          .name
+            margin-left 6px
+            line-height 18px
+            font-size 16px
+            font-weight bold
+
+
+        .description
+          font-size 12px
+          font-weight 200
+          line-height 12px
+          margin-bottom 10px
+
+        .support
+          .icon
+            display inline-block
+            vertical-align top
+            width 12px
+            height 12px
+            margin-right 4px
+            background-size 12px 12px
+            background-repeat no-repeat
+            &.decrease
+              bg-image('decrease_1')
+            &.discount
+              bg-image('discount_1')
+            &.guarantee
+              bg-image('guarantee_1')
+            &.invoice
+              bg-image('invoice_1')
+            &.special
+              bg-image('special_1')
+          .text
+            font-size 10px
+            font-weight 200
+            line-height 12px
+
+        .support-count
+          position absolute
+          right 12px
+          bottom 14px
+          padding 7px 8px
+          border-radius 14px
+          background rgba(0, 0, 0, 0.2)
+          .count
+            vertical-align top
+            font-size 10px
+            font-weight 200
+          .icon-keyboard_arrow_right
+            margin-left 2px
+            font-size 10px
+
+    .bulletin-wrapper
+      position relative
+      height 28px
+      line-height 28px
+      padding 0 22px 0 12px
+      white-space nowrap
+      overflow hidden
+      text-overflow ellipsis
+      background rgba(7, 17, 27, 0.2)
+      .bulletin-title
+        display inline-block
+        vertical-align top
+        margin-top 8px
+        width 22px
+        height 12px
+        bg-image('bulletin')
+        background-size 22px 12px
+        background-repeat no-repeat
+      .bulletin-text
+        vertical-align top
+        font-size 10px
+        margin 0 4px
+      .icon-keyboard_arrow_right
+        position absolute
+        font-size 10px
+        right 12px
+        top 10px
+    .background
+      position absolute
+      top 0
+      left 0
+      width 100%
+      height 100%
+      z-index: -1
+      filter blur(10px)
 
 </style>
