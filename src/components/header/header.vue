823a9169ea0fda7a06c5e1da5c32d19cde61b373
OneE <774247851@qq.com>
2017-05-06 22:50:03 +0800

    Completed header component.

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -30,19 +30,40 @@
     <div class="background">
       <img :src="seller.avatar" width="100%" height="100%">
     </div>
-    <div v-show="detailShow" class="detail">
-      <div class="detail-wrapper clearfix">
-        <div class="detail-main">
-          <h1 class="name">{{seller.name}}</h1>
-          <div class="star-wrapper">
-            <oe-star :size="48" :score="seller.score"></oe-star>
+    <transition name="fade">
+      <div v-show="detailShow" class="detail">
+        <div class="detail-wrapper clearfix">
+          <div class="detail-main">
+            <h1 class="name">{{seller.name}}</h1>
+            <div class="star-wrapper">
+              <oe-star :size="48" :score="seller.score"></oe-star>
+            </div>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">优惠信息</div>
+              <div class="line"></div>
+            </div>
+            <ul v-if="seller.supports" class="support">
+              <li class="support-item" v-for="item in seller.supports">
+                <sapn class="icon" :class="classMap[item.type]"></sapn>
+                <span class="text">{{item.description}}</span>
+              </li>
+            </ul>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">商家公告</div>
+              <div class="line"></div>
+            </div>
+            <div class="bulletin">
+              <p class="content">{{seller.bulletin}}</p>
+            </div>
           </div>
         </div>
+        <div class="detail-close" @click="closeDetail">
+          <i class="icon-close"></i>
+        </div>
       </div>
-      <div class="detail-close" @click="closeDetail">
-        <i class="icon-close"></i>
-      </div>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -203,7 +224,13 @@
       top 0
       left 0
       overflow auto
+      backdrop-filter: blur(10px)
       background rgba(7, 17, 27, 0.8)
+      opacity 1
+      &.fade-enter-active, &.fade-leave-active
+        transition all 0.5s
+      &.fade-enter, &.fade-leave-active
+        opacity 0
       .detail-wrapper
         width 100%
         min-height 100%
@@ -219,6 +246,56 @@
             margin-top 16px
             padding 2px 0
             text-align center
+          .title
+            display flex
+            width 80%
+            margin 28px auto 24px auto
+            .line
+              flex 1
+              position relative
+              top: -6px
+              border-bottom 1px solid rgba(255, 255, 255, 0.2)
+            .text
+              padding 0 12px
+              font-weight 700
+              font-size 14px
+          .support
+            width 80%
+            margin 0 auto
+            .support-item
+              padding 0 12px
+              margin-bottom 12px
+              font-size 0
+              &:last-child
+                margin-bottom 0
+              .icon
+                display inline-block
+                width 16px
+                height 16px
+                vertical-align top
+                margin-right 6px
+                background-size 16px
+                background-repeat no-repeat
+                &.decrease
+                  bg-image('decrease_2')
+                &.discount
+                  bg-image('discount_2')
+                &.guarantee
+                  bg-image('guarantee_2')
+                &.invoice
+                  bg-image('invoice_2')
+                &.special
+                  bg-image('special_2')
+              .text
+                line-height 16px
+                font-size 12px
+          .bulletin
+            width 80%
+            margin 0 auto
+            .content
+              padding 0 12px
+              line-height 24px
+              font-size 12px
       .detail-close
         position relative
         font-size 32px
@@ -226,5 +303,4 @@
         height 32px
         margin -64px auto 0 auto
         color rgba(255, 255, 255, 0.5)
-
 </style>
