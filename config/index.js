de53dcf6119973cda9278ff10d24ae550e16feb2
wangyu <wangyu@qiumiapp.com>
2021-05-26 18:23:41 +0800

    feat: add docs

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
     // 生产环境不允许调试
     productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
